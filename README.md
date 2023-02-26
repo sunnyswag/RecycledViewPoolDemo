80776521f96bf17188c260fd34e63bf1d1813e28
sunnyswag <hhq126152@gmail.com>
2023-02-26 15:58:34 +0800

    RecyclerView, recycledViewPool 的多个 viewType 的实现

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RecycledViewPoolDemo
 RecycledViewPool 使用时的 Demo
+
+
+
+## Reference
+
+https://gist.github.com/yrom/728237025575005a8fd3
+
+https://www.jianshu.com/p/122e68e9ddac
--- app/src/main/java/com/example/recycledviewpooldemo/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recycledviewpooldemo/adapter/RecyclerViewAdapter.kt
@@ -4,23 +4,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recycledviewpooldemo.R
 import com.example.recycledviewpooldemo.databinding.RecyclerviewItemBinding
 
-class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding = RecyclerviewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         binding.tvOnCreate.apply {
             text = "onCreateViewHolder invoke, viewType: $viewType"
             visibility = View.VISIBLE
         }
-        return ViewHolder(binding)
+        return if (viewType == VIEW_TYPE_BLUE) {
+            BlueViewHolder(binding)
+        } else {
+            PurpleViewHolder(binding)
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvOnBind.apply {
-            text = "onBindViewHolder invoke, position: $position"
-            visibility = View.VISIBLE
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is BlueViewHolder) {
+            holder.binding.tvOnBind.text = "onBindViewHolder invoke, position: $position"
+            holder.binding.tvOnBind.visibility = View.VISIBLE
+        } else if (holder is PurpleViewHolder) {
+            holder.binding.tvOnBind.text = "onBindViewHolder invoke, position: $position"
+            holder.binding.tvOnBind.visibility = View.VISIBLE
         }
     }
 
@@ -28,16 +36,42 @@ class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(
         return ITEM_COUNT
     }
 
-    override fun onViewDetachedFromWindow(holder: ViewHolder) {
-        super.onViewDetachedFromWindow(holder)
-        holder.binding.tvOnBind.visibility = View.INVISIBLE
-        holder.binding.tvOnCreate.visibility = View.INVISIBLE
+    override fun getItemViewType(position: Int): Int {
+        val middle = ITEM_COUNT / 2
+        return if (position <= middle) {
+            VIEW_TYPE_BLUE
+        } else {
+            VIEW_TYPE_PURPLE
+        }
     }
 
-    class ViewHolder(val binding: RecyclerviewItemBinding) : RecyclerView.ViewHolder(binding.root)
+    override fun onViewDetachedFromWindow(holder: RecyclerView.ViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        if (holder is BlueViewHolder) {
+            holder.binding.tvOnCreate.visibility = View.INVISIBLE
+            holder.binding.tvOnBind.visibility = View.INVISIBLE
+        } else if (holder is PurpleViewHolder) {
+            holder.binding.tvOnCreate.visibility = View.INVISIBLE
+            holder.binding.tvOnBind.visibility = View.INVISIBLE
+        }
+    }
+
+    class BlueViewHolder(val binding: RecyclerviewItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setBackgroundResource(R.color.blue_test)
+        }
+    }
+    class PurpleViewHolder(val binding: RecyclerviewItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setBackgroundResource(R.color.purple_test)
+        }
+    }
 
     companion object {
         private const val TAG = "RecyclerViewAdapter"
-        const val ITEM_COUNT = 20
+        const val ITEM_COUNT = 40
+
+        const val VIEW_TYPE_BLUE = 0
+        const val VIEW_TYPE_PURPLE = 1
     }
 }
